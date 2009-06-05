5c503860ba43b2bc144337e576654f7cdb9718e7
Jianing Yang <detrox@gmail.com>
2009-06-06 01:05:10 +0800

    bug fixed

--- trie.cc
+++ trie.cc
@@ -8,8 +8,10 @@
 // * Implementation of basic_trie                                         *
 // ************************************************************************
 
-basic_trie::basic_trie(size_type size)
-    :header_(NULL), states_(NULL), last_base_(0), owner_(true)
+basic_trie::basic_trie(size_type size, 
+                       trie_relocator_interface<size_type> *relocator)
+    :header_(NULL), states_(NULL), last_base_(0), owner_(true),
+     relocator_(relocator)
 {
     if (size < kCharsetSize)
         size = kCharsetSize;
@@ -21,14 +23,16 @@ basic_trie::basic_trie(size_type size)
 }
 
 basic_trie::basic_trie(void *header, void *states)
-    :header_(NULL), states_(NULL), last_base_(0), owner_(false)
+    :header_(NULL), states_(NULL), last_base_(0), owner_(false),
+     relocator_(NULL)
 {
     header_ = static_cast<header_type *>(header);
     states_ = static_cast<state_type *>(states);
 }
 
 basic_trie::basic_trie(const basic_trie &trie)
-    :header_(NULL), states_(NULL), last_base_(0), owner_(false)
+    :header_(NULL), states_(NULL), last_base_(0), owner_(false),
+     relocator_(NULL)
 {
     clone(trie);
 }
@@ -96,7 +100,7 @@ basic_trie::find_base(const char_type *inputs, const extremum_type &extremum)
                 }
             }
         } else {
-            // i += min
+            //i += extremum.min;
         }
     }
 
@@ -120,7 +124,6 @@ basic_trie::relocate(size_type stand,
     for (i = 0; inputs[i]; i++) {
         if (check(obase + inputs[i]) != s)  // find old links
             continue;
-        // create new links according old ones
         set_base(nbase + inputs[i], base(obase + inputs[i]));
         set_check(nbase + inputs[i], check(obase + inputs[i]));
         find_exist_target(obase + inputs[i], targets, NULL);
@@ -130,9 +133,12 @@ basic_trie::relocate(size_type stand,
         // if where we are standing is moving, we move with it
         if (stand == obase + inputs[i])
             stand = nbase + inputs[i];
+        if (relocator_)
+            relocator_->relocate(obase + inputs[i], nbase + inputs[i]);
         // free old places
         set_base(obase + inputs[i], 0);
         set_check(obase + inputs[i], 0);
+        // create new links according old ones
     }
     // finally, set new base
     set_base(s, nbase);
@@ -141,7 +147,7 @@ basic_trie::relocate(size_type stand,
 }
 
 basic_trie::size_type
-basic_trie::create_link(size_type s, char_type ch)
+basic_trie::create_transition(size_type s, char_type ch)
 {
     char_type targets[kCharsetSize + 1];
     char_type parent_targets[kCharsetSize + 1];
@@ -158,7 +164,6 @@ basic_trie::create_link(size_type s, char_type ch)
             find_exist_target(s, targets, &extremum);
         size_type num_parent_targets = check(t)?
             find_exist_target(check(t), parent_targets, &parent_extremum):0;
-
         if (num_parent_targets > 0 && num_targets + 1 > num_parent_targets) {
             s = relocate(s, check(t), parent_targets, parent_extremum);
         } else {
@@ -195,11 +200,11 @@ void basic_trie::insert(const char *inputs, size_t length, value_type val)
 
     for (/* empty */; p < inputs + length; p++) {
         char_type ch = char_in(*p);
-        s = create_link(s, ch);
+        s = create_transition(s, ch);
     }
 
     // add a terminator
-    s = create_link(s, kTerminator);
+    s = create_transition(s, kTerminator);
     set_base(s, val);
 }
 
@@ -237,7 +242,7 @@ void basic_trie::trace(size_type s) const
             cbase = base(*it);
             if (obase) {
                 if (*it - obase == kTerminator) {
-                    std::cerr << "-'#'->";
+                    std::cerr << "-#->";
                 } else {
                     char ch = char_out(*it - obase);
                     if (isalnum(ch))
@@ -259,12 +264,17 @@ void basic_trie::trace(size_type s) const
 // * Implementation of trie                                               *
 // ************************************************************************
 
-trie::trie():
-    lhs_(NULL), rhs_(NULL), index_(NULL), accept_(NULL),
-    index_size_(0), accept_size_(0)
+trie::trie()
+    :lhs_(NULL), rhs_(NULL), index_(NULL), accept_(NULL),
+     index_size_(0), accept_size_(0), next_accept_(1), next_index_(1),
+     front_relocator_(NULL), rear_relocator_(NULL), stand_(0)
 {
+    front_relocator_ = new trie_relocator<trie>(this, &trie::relocate_front);
+    rear_relocator_ = new trie_relocator<trie>(this, &trie::relocate_rear);
     lhs_ = new basic_trie();
     rhs_ = new basic_trie();
+    lhs_->set_relocator(front_relocator_);
+    rhs_->set_relocator(rear_relocator_);
 }
 
 trie::~trie()
@@ -287,6 +297,187 @@ trie::~trie()
         accept_ = NULL;
         accept_size_ = 0;
     }
+    if (front_relocator_) {
+        delete front_relocator_;
+        front_relocator_ = NULL;
+    }
+    if (rear_relocator_) {
+        delete rear_relocator_;
+        rear_relocator_ = NULL;
+    }
+}
+
+basic_trie::size_type
+trie::rhs_append(const char *inputs, size_t length)
+{
+    const char *p = NULL;
+    size_type s = 1, t;
+
+    t = rhs_->next(s, basic_trie::kTerminator);
+    if (rhs_->check_transition(s, t)) {
+        s = rhs_->go_forward_reverse(t, inputs, length, &p);
+        if (p < inputs) {  // all characters match
+            t = rhs_->next(s, basic_trie::kTerminator);
+            if (outdegree(s) == 0)
+                return s;
+            else if (rhs_->check_transition(s, t))
+                return rhs_->next(s, basic_trie::kTerminator);
+            else
+                return rhs_->create_transition(s, basic_trie::kTerminator);
+        }
+    }
+    if (outdegree(s) == 0) {
+        t = rhs_->create_transition(s, basic_trie::kTerminator);
+        std::set<size_type>::const_iterator it;
+        for (it = refer_[s].referer.begin();
+                it != refer_[s].referer.end();
+                it++) {
+            set_link(*it, t);
+        }
+        refer_.erase(s);
+    }
+    if (s == 1) {
+        p = inputs + length - 1;
+        s = rhs_->create_transition(s, basic_trie::kTerminator);
+    }
+    for (; p >= inputs; p--) {
+        t = rhs_->create_transition(s, basic_trie::char_in(*p));
+        s = t;
+    }
+    return s;
+}
+
+void trie::lhs_insert(size_type s, const char *inputs, size_t length)
+{
+    size_type t = lhs_->create_transition(s, basic_trie::char_in(inputs[0]));
+    set_link(t, rhs_append(inputs + 1, length - 1));
+}
+
+void trie::rhs_clean_more(size_type t)
+{
+    assert(t > 0);
+    if (outdegree(t) == 0 && count_referer(t) == 0) {
+        size_type s = rhs_->prev(t);
+        remove_accept_state(t);
+        if (s > 0)
+            rhs_clean_more(s);
+    } else if (outdegree(t) == 1) {
+        size_type r = rhs_->next(t, basic_trie::kTerminator);
+        if (rhs_->check_transition(t, r)) {
+            // delete transition 't -#-> r'
+            std::set<size_type>::const_iterator it;
+            for (it = refer_[r].referer.begin();
+                    it != refer_[r].referer.end();
+                    it++) 
+                set_link(*it, t);
+//            if (refer_.find(t) != refer_.end())
+//                assert (accept_[refer_[t].accept_index].accept == r || accept_[refer_[t].accept_index].accept == t);
+            accept_[refer_[t].accept_index].accept = t;
+            remove_accept_state(r);
+        }
+    }
+}
+
+void trie::rhs_insert(size_type s, size_type r, 
+                      const char *match, size_t match_length,
+                      const char *remain, size_t remain_length,
+                      char ch, bool terminator)
+{
+    // R-1
+    size_type u = link_state(s); // u might be zero
+    index_[-lhs_->base(s)].index = 0;
+    lhs_->set_base(s, 0); // XXX: Bug?
+    stand_ = r;
+    if (u > 0) {
+        refer_[u].referer.erase(s);
+
+        if (refer_[u].referer.size() == 0)
+            refer_.erase(u);
+    }
+
+    // R-2
+    const char *p;
+    size_type t;
+    //printf("match_length = %d\n", match_length);
+    for (p = match; p < match + match_length; p++) {
+        t = lhs_->create_transition(s, basic_trie::char_in(*p));
+        s = t;
+    }
+    t = lhs_->create_transition(s, basic_trie::char_in(*remain));
+    set_link(t, rhs_append(remain + 1, remain_length - 1));
+
+    // R-3
+    t = lhs_->create_transition(s, (terminator)?basic_trie::kTerminator:
+                                                basic_trie::char_in(ch));
+    size_type v = rhs_->prev(stand_); // v -ch-> r
+    if (!rhs_->check_transition(v, rhs_->next(v, basic_trie::kTerminator)))
+        r = rhs_->create_transition(v, basic_trie::kTerminator);
+    else
+        r = rhs_->next(v, basic_trie::kTerminator);
+
+    set_link(t, r);
+
+    // R-4
+    if (u > 0) {
+        if (!rhs_clean_one(u))
+            rhs_clean_more(u);
+    }
+
+}
+
+void trie::insert(const char *inputs, size_t length)
+{
+    size_type s;
+    const char *p;
+    s = lhs_->go_forward(1, inputs, length, &p);
+    if (p < inputs + length && !check_separator(s)) {
+        lhs_insert(s, p, length - (p - inputs));
+        return;
+    }
+    if (p >= inputs + length)
+        return;
+
+    size_type r = link_state(s);
+    if (rhs_->check_reverse_transition(r, basic_trie::kTerminator))
+        r = rhs_->prev(r);
+
+    char last;
+    bool terminator = false;
+    exists_.clear();
+    for (; p < inputs + length; p++) {
+        if (rhs_->check_reverse_transition(r, basic_trie::char_in(*p))) {
+            r = rhs_->prev(r);
+            exists_.append(1, *p);
+        } else {
+            last = basic_trie::char_out(r - rhs_->base(rhs_->prev(r)));
+            terminator = (r - rhs_->base(rhs_->prev(r)) == basic_trie::kTerminator)?true:false;
+            break;
+        }
+    }
+    if (p < inputs + length)
+        rhs_insert(s, r, exists_.c_str(), exists_.length(),
+                   p, length - (p - inputs), last, terminator);
+    return;
+}
+
+int trie::search(const char *inputs, size_t length)
+{
+    size_type s;
+    const char *p;
+    s = lhs_->go_forward(1, inputs, length, &p);
+    if (p < inputs + length && !check_separator(s))
+        return -1;
+    if (p >= inputs + length)
+        return true;
+    size_type r = link_state(s);
+    if (rhs_->check_reverse_transition(r, basic_trie::kTerminator))
+        r = rhs_->prev(r);
+
+    r = rhs_->go_backward(r, p, length - (p - inputs), NULL);
+    r = rhs_->prev(r);    
+    if (r == 1)
+        return true;
+    return -2;
 }
 
 // vim: ts=4 sw=4 ai et
--- trie.h
+++ trie.h
@@ -24,7 +24,14 @@
 #include <stdexcept>
 #include <vector>
 #include <cassert>
+#include <string>
 #include <map>
+#include <set>
+
+template<typename T> class trie_relocator_interface {
+  public:
+    virtual void relocate(T s, T t) = 0;
+};
 
 class basic_trie
 {
@@ -51,7 +58,8 @@ class basic_trie
     static const char_type kCharsetSize = 257;
     static const char_type kTerminator = kCharsetSize;
 
-    explicit basic_trie(size_type size = 1024);
+    explicit basic_trie(size_type size = 1024,
+                        trie_relocator_interface<size_type> *relocator = NULL);
     basic_trie(const basic_trie &trie);
     void clone(const basic_trie &trie);
     basic_trie &operator=(const basic_trie &trie);
@@ -59,12 +67,17 @@ class basic_trie
     void insert(const char *inputs, size_t length, value_type val);
     value_type search(const char *inputs, size_t length) const;
     void trace(size_type s) const;
-    size_type create_link(size_type s, char_type ch);
+    size_type create_transition(size_type s, char_type ch);
 
     static const basic_trie *create_from_memory(void *header,
                                                 void *states)
     {
         return new basic_trie(header, states);
+    }
+
+    void set_relocator(trie_relocator_interface<size_type> *relocator)
+    {
+        relocator_ = relocator;
     }
 
     value_type base(size_type s) const
@@ -119,12 +132,49 @@ class basic_trie
         return s;
     }
 
+    size_type go_forward_reverse(size_type s,
+                                 const char *inputs,
+                                 size_t length,
+                                 const char **mismatch) const
+    {
+        const char *p;
+        for (p = inputs + length - 1; p >= inputs; p--) {
+            char_type ch = char_in(*p);
+            size_type t = next(s, ch);
+            if (!check_transition(s, t))
+                break;
+            s = t;
+        }
+        if (mismatch)
+            *mismatch = p;
+        return s;
+    }
+
     // Go backward from state s with inputs, returns the last
     // states and the mismatch position by pointer
     size_type go_backward(size_type s,
                           const char *inputs,
                           size_t length,
                           const char **mismatch) const
+    {
+        const char *p;
+        for (p = inputs; p < inputs + length; p++) {
+            char_type ch = char_in(*p);
+            size_type t = prev(s);
+            if (!check_transition(t, next(t, ch)))
+                break;
+            s = t;
+        }
+        if (mismatch)
+            *mismatch = p;
+
+        return s;
+    }
+
+    size_type go_backward_reverse(size_type s,
+                                  const char *inputs,
+                                  size_t length,
+                                  const char **mismatch) const
     {
         const char *p;
         for (p = inputs + length - 1; p >= inputs; p--) {
@@ -138,6 +188,64 @@ class basic_trie
             *mismatch = p;
 
         return s;
+    }
+
+    const header_type *header() const
+    {
+        return header_;
+    }
+
+    const state_type *states() const
+    {
+        return states_;
+    }
+
+    bool owner() const
+    {
+        return owner_;
+    }
+
+    bool check_transition(size_type s, size_type t) const
+    {
+        return (s > 0 && t > 0 && t < header_->size && check(t) == s)?true:false;
+    }
+    bool check_reverse_transition(size_type s, char_type ch) const
+    {
+        return ((next(prev(s), ch) == s) 
+                && check_transition(prev(s), next(prev(s), ch)));
+    }
+
+    static char_type char_in(const char ch)
+    {
+        return static_cast<unsigned char>(ch + 1);
+    }
+
+    static char char_out(char_type ch)
+    {
+        return static_cast<char>(ch - 1);
+    }
+
+  protected:
+    explicit basic_trie(void *header, void *states);
+    size_type find_base(const char_type *inputs,
+                        const extremum_type &extremum);
+    size_type relocate(size_type stand,
+                       size_type s,
+                       const char_type *inputs,
+                       const extremum_type &extremum);
+
+    void inflate(size_type size)
+    {
+        // align with 4k
+        size_type nsize = (((header_->size + size) >> 12) + 1) << 12;
+        states_ = static_cast<state_type *>(realloc(states_,
+                                                  nsize * sizeof(state_type)));
+        if (!states_)
+            throw std::bad_alloc();
+        memset(states_ + header_->size,
+               0,
+               (nsize - header_->size) * sizeof(state_type));
+        header_->size = nsize;
     }
 
     // Find out all exists targets from s and store them into *targets.
@@ -169,65 +277,34 @@ class basic_trie
 
         return p - targets;
     }
-
-    const header_type *header() const
-    {
-        return header_;
-    }
-
-    const state_type *states() const
-    {
-        return states_;
-    }
-
-    bool owner() const
-    {
-        return owner_;
-    }
-
-  protected:
-    explicit basic_trie(void *header, void *states);
-    size_type find_base(const char_type *inputs,
-                        const extremum_type &extremum);
-    size_type relocate(size_type stand,
-                       size_type s,
-                       const char_type *inputs,
-                       const extremum_type &extremum);
-
-    bool check_transition(size_type s, size_type t) const
-    {
-        return (t > 0 && t < header_->size && check(t) == s)?true:false;
-    }
-
-    char_type char_in(const char ch) const
-    {
-        return static_cast<unsigned char>(ch + 1);
-    }
-
-    char char_out(char_type ch) const
-    {
-        return static_cast<char>(ch - 1);
-    }
-
-    void inflate(size_type size)
-    {
-        // align with 4k
-        size_type nsize = (((header_->size + size) >> 12) + 1) << 12;
-        states_ = static_cast<state_type *>(realloc(states_,
-                                                  nsize * sizeof(state_type)));
-        if (!states_)
-            throw std::bad_alloc();
-        memset(states_ + header_->size,
-               0,
-               (nsize - header_->size) * sizeof(state_type));
-        header_->size = nsize;
-    }
-
   private:
     header_type *header_;
     state_type *states_;
     size_type last_base_;
     bool owner_;
+    trie_relocator_interface<size_type> *relocator_;
+};
+
+template<typename T>
+class trie_relocator: public trie_relocator_interface<basic_trie::size_type> {
+  public:
+    typedef basic_trie::size_type size_type;
+    typedef void (T::*relocate_function)(size_type, size_type);
+    trie_relocator(T *who, relocate_function relocate)
+        :who_(who), relocate_(relocate)
+    {
+    }
+
+    void relocate(size_type s, size_type t)
+    {
+        (who_->*relocate_)(s, t);
+    }
+  private:
+    T *who_;
+    relocate_function relocate_;
+
+    trie_relocator(const trie_relocator &);
+    void operator=(const trie_relocator &);
 };
 
 class trie {
@@ -235,12 +312,41 @@ class trie {
     typedef basic_trie::size_type size_type;
     trie();
     ~trie();
+    void insert(const char *inputs, size_t length);
+    int search(const char *inputs, size_t length);
+    const basic_trie *front_trie() const
+    {
+        return lhs_;
+    }
+    const basic_trie *rear_trie() const
+    {
+        return rhs_;
+    }
+
 //  protected:
+    size_type rhs_append(const char *inputs, size_t length);
+    void lhs_insert(size_type s, const char *inputs, size_t length);
+    void rhs_clean_more(size_type t);
+    void rhs_insert(size_type s, size_type r, 
+                    const char *match, size_t match_length,
+                    const char *remain, size_t remain_length,
+                    char ch, bool terminator);
+
+    void remove_accept_state(size_type s)
+    {
+        assert(s > 0);
+        rhs_->set_base(s, 0);
+        rhs_->set_check(s, 0);
+        if (count_referer(s) == 0)
+            accept_[refer_[s].accept_index].accept = 0;
+        refer_.erase(s);
+    }
+
     bool check_separator(size_type s) const
     {
         return (lhs_->base(s) < 0)?true:false;
     }
-    
+
     size_type link_state(size_type s) const
     {
         return accept_[index_[-lhs_->base(s)].index].accept;
@@ -248,78 +354,156 @@ class trie {
 
     void set_link(size_type s, size_type t)
     {
-        if (backref_[t]) {
-            index_[index_entry(s)].index = backref_[t];
+        size_type i;
+
+        if (refer_.find(t) != refer_.end()) {
+            i = find_index_entry(s);
+            index_[i].index = refer_[t].accept_index;
         } else {
-            size_type i = accept_entry(index_entry(s));
+            i = find_accept_entry(find_index_entry(s));
             accept_[i].accept = t;
-            accept_[i].count++;
-            backref_[t] = i;
+            refer_[t].accept_index = i;
         }
+        assert(lhs_->base(s) < 0);
+        refer_[t].referer.insert(s);
     }
-    
-    size_type index_entry(size_type s)
+
+    size_t count_referer(size_type s) const
     {
-        static size_type next = 1;
-        if (!lhs_->base(s)) {
-            if (next >= index_size_) {
-                size_type nsize = next * 2 + 1;
+        std::map<size_type, refer_type>::const_iterator found(refer_.find(s));
+        if (found == refer_.end())
+            return 0;
+        else 
+            return found->second.referer.size();
+    }
+
+    size_type find_index_entry(size_type s)
+    {
+        assert(lhs_->base(s) <= 0);
+        if (lhs_->base(s) >= 0) {
+            if (next_index_ >= index_size_) {
+                size_type nsize = next_index_ * 2;
+                //size_type nsize = (((index_size_ + next) >> 12) + 1) << 12;
                 index_ = static_cast<index_type *>
                         (realloc(index_, nsize * sizeof(index_type)));
                 if (!index_)
                     throw std::bad_alloc();
                 memset(index_ + index_size_, 0,
                        (nsize - index_size_) * sizeof(index_type));
-                index_size_ = nsize; 
+                index_size_ = nsize;
             }
-            lhs_->set_base(s, -next);
-            ++next;
+            //printf("index s = %d\n", s);
+            lhs_->set_base(s, -next_index_);
+            ++next_index_;
         }
         return -lhs_->base(s);
     }
 
-    size_type accept_entry(size_type i)
+    size_type find_accept_entry(size_type i)
     {
-        static size_type next = 1;
         if (!index_[i].index) {
-            if (next >= accept_size_) {
-                size_type nsize = next * 2 + 1;
+            if (next_accept_ >= accept_size_) {
+                size_type nsize = next_accept_ * 2;
                 accept_ = static_cast<accept_type *>
                         (realloc(accept_, nsize * sizeof(accept_type)));
                 if (!accept_)
                     throw std::bad_alloc();
                 memset(accept_ + accept_size_, 0,
                        (nsize - accept_size_) * sizeof(accept_type));
-                accept_size_ = nsize; 
+                accept_size_ = nsize;
             }
-            index_[i].index = next++;
+            //printf("accept i = %d\n", i);
+            index_[i].index = next_accept_;
+            ++next_accept_;
         }
         return index_[i].index;
+    }
+
+    size_t outdegree(size_type s) const
+    {
+        basic_trie::char_type ch;
+        size_t degree = 0;
+        for (ch = 1; ch < basic_trie::kCharsetSize + 1; ch++) {
+            size_type t = rhs_->next(s, ch);
+            if (t >= rhs_->header()->size)
+                break;
+            if (rhs_->check_transition(s, t))
+                degree++;
+        }
+
+        return degree;
+    }
+
+    bool rhs_clean_one(size_type t)
+    {
+        size_type s = rhs_->prev(t);
+        if (s > 0
+            && t == rhs_->next(s, basic_trie::kTerminator)
+            && count_referer(t) == 0) {
+            // delete one
+            remove_accept_state(t);
+            return true;
+        }
+        return false;
     }
 
     void trace_table(size_type istart,
                      size_type astart) const
     {
-        static const size_type dsize = 10;
+        static const size_type dsize = 40;
         size_type i;
         printf("========================================");
         printf("\nSEQ     |");
         for (i = istart; i < dsize && i < index_size_; i++)
-            printf("%2d ", i);
+            printf("%4d ", i);
         printf("\nDATA    |");
         for (i = istart; i < dsize && i < index_size_; i++)
-            printf("%2d ", index_[i].data);
+            printf("%4d ", index_[i].data);
         printf("\nINDEX   |");
         for (i = istart; i < dsize && i < index_size_; i++)
-            printf("%2d ", index_[i].index);
+            printf("%4d ", index_[i].index);
         printf("\nCOUNT   |");
         for (i = astart; i < dsize && i < accept_size_; i++)
-            printf("%2d ", accept_[i].count);
+            printf("%4d ", count_referer(accept_[i].accept));
         printf("\nACCEPT  |");
         for (i = astart; i < dsize && i < accept_size_; i++)
-            printf("%2d ", accept_[i].accept);
+            printf("%4d ", accept_[i].accept);
         printf("\n========================================\n");
+        std::set<size_type>::const_iterator it;       
+        std::map<size_type, refer_type>::const_iterator mit;
+        for (mit = refer_.begin(); mit != refer_.end(); mit++) {
+            printf("%4d: ", mit->first);
+            for (it = mit->second.referer.begin();
+                 it != mit->second.referer.end();
+                 it++)
+                printf("%4d ", *it);
+            printf("\n");
+        }
+        printf("========================================\n");
     }
+
+    void relocate_front(size_type s, size_type t)
+    {
+        if (lhs_->base(s) < 0) {
+            size_type r = link_state(s);
+            if (refer_.find(r) != refer_.end()) {
+                refer_[r].referer.erase(s);
+                assert(lhs_->base(t) < 0);
+                refer_[r].referer.insert(t);
+            }
+        }
+    }    
+
+    void relocate_rear(size_type s, size_type t)
+    {
+        if (refer_.find(s) != refer_.end()) {
+            accept_[refer_[s].accept_index].accept = t;
+            refer_[t] = refer_[s];
+            refer_.erase(s);
+        } else if (stand_ == s) {
+            stand_ = t;
+        }
+    }    
 
   private:
     typedef struct {
@@ -327,16 +511,24 @@ class trie {
         size_type index;
     } index_type;
     typedef struct {
-        size_type count;
         size_type accept;
     } accept_type;
+    typedef struct {
+        size_type accept_index;
+        std::set<size_type> referer;
+    } refer_type;
 
     basic_trie *lhs_, *rhs_;
     index_type *index_;
     accept_type *accept_;
     size_type index_size_, accept_size_;
-    std::map<size_type, size_type> backref_;
+    std::map<size_type, refer_type> refer_;
+    std::string exists_;
+    size_type next_accept_, next_index_;
+    trie_relocator<trie> *front_relocator_, *rear_relocator_;
+    size_type stand_;
 };
+
 #endif  // TRIE_H_
 
 // vim: ts=4 sw=4 ai et
